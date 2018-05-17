35ac151ecedb0552ee49270f7ac945dd7b69a4ba
Glen Pegado <glenpegado@gmail.com>
2018-05-16 18:33:54 -0700

    added channels

--- chatigloo-frontend/src/components/messenger.js
+++ chatigloo-frontend/src/components/messenger.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react'
-
+import classNames from 'classnames'
+import avatar from '../images/avatar.png'
 import '../css/app.css'
+import {OrderedMap} from 'immutable'
+import _ from 'lodash'
 
 export default class Messenger extends Component{
     constructor(props){
@@ -8,9 +11,10 @@ export default class Messenger extends Component{
       super(props);
 
       this.state = {
-        height: window.innerHeight
+        height: window.innerHeight,
       }
       this._onResize = this._onResize.bind(this)
+      this.addTestMessages = this.addTestMessages.bind(this)
     }
 
     _onResize(){
@@ -22,40 +26,177 @@ export default class Messenger extends Component{
     componentDidMount(){
 
       window.addEventListener('resize', this._onResize)
+
+      this.addTestMessages();
+
     }
 
-    componentWillUnmount(){
+    addTestMessages(){
+      //same as const store = this.props.store
+      const {store} = this.props;
 
+
+      // create test messages
+      for(let i = 0; i < 100; i++){
+          let isMe = false;
+
+          if(i % 3 === 0){
+            isMe = true
+          }
+
+          const newMsg = {
+            _id: i,
+            author: `Author: ${i}`,
+            body: `The body of message ${i}`,
+            avatar: avatar,
+            me: isMe,
+          }
+          store.addMessage(i, newMsg);
+      }
+
+
+      //create some test channels
+      for(let c = 0; c < 10; c++){
+        let newChannel = {
+          _id: `${c}`,
+          title: `Channel title ${c}`,
+          lastMessage: `Hey there here..${c}`,
+          members: new OrderedMap({
+            '2': true,
+            '3': true,
+            '1': true,
+          }),
+          messages: new OrderedMap(),
+        }
+
+        const msgId = `${c}`;
+        const moreMsgId = `${c+1}`
+        newChannel.messages = newChannel.messages.set(msgId, true)
+        newChannel.messages = newChannel.messages.set(moreMsgId, true)
+
+        store.addChannel(c, newChannel)
+
+    }
+
+  }
+
+    componentWillUnmount(){
       window.removeEventListener('resize', this._onResize)
     }
 
     render(){
+      const {store} = this.props
+
       const {height} = this.state
+
       const style= {
-        height: 600
-      }
+        height: height,
+      };
+
+        const activeChannel = store.getActiveChannel();
+        const messages = store.getMessagesFromChannel(activeChannel)
+        const channels = store.getChannels()
+        const members = store.getMembersFromChannel(activeChannel)
+
+        console.log("Active channel is:", activeChannel)
+
         return (
           <div style ={style} className="app-messenger">
               <div className="header">
                 <div className="left">
-                  <div className="">New message</div>
+                  <div className="actions">
+                    <button>New message</button>
+                  </div>
                 </div>
                 <div className="content"><h2>Title</h2></div>
                 <div className="right"></div>
 
                   <div className="user-bar">
-                    <div className="profile-name">Glen Pegado      </div>
-                    <div className="profile-image"><img src="http://0.gravatar.com/avatar/9c47cb576f4045ac73d146277b2b6528?s=50&d=mm&r=g" alt=""/></div>
+                    <div className="profile-name">Glen Pegado</div>
+                    <div className="profile-image"><img src={avatar} alt=""/></div>
                 </div>
               </div>
-
-
               <div className="main">
-                <div className="sidebar-left">Left sidebar</div>
-                <div className="content">Content</div>
-                <div className="sidebar-right">Right sidebar</div>
+                <div className="sidebar-left">
+                  <div className="channels">
+
+                    {channels.map((channel, key) => {
+                      return (
+                        <div onClick={(key) =>
+
+                          store.setActiveChannelId(channel._id)
+                        }
+                        key={channel._id} className="channel">
+                          <div className="user-image">
+                            <img src={avatar} alt=""/>
+                          </div>
+                          <div className="channel-info">
+                            <h2>{channel.title}</h2>
+                            <p>{channel.lastMessage}</p>
+                          </div>
+                        </div>
+                      )
+                    })}
+
+
+
+                  </div>
+                </div>
+                <div className="content">
+                  <div className="messages">
+                    {messages.map((message, index) => {
+
+                        return (
+                          <div key={index} className={classNames('message', {'me': message.me})}>
+                            <div className="message-user-image">
+                              <img src={message.avatar} alt=""/>
+                            </div>
+                            <div className="message-body">
+                              <div className="message-author">{message.me ? 'You' : message.author} says:</div>
+                              <div className="message-text">
+                                <p>
+                                  {message.body}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        )
+                      })}
+                  </div>
+                  <div className="message-input">
+                    <div className="text-input">
+                      <textarea placeholder= "Type here..." />
+                    </div>
+
+                    <div className="actions">
+                      <button className="send">Send</button>
+                    </div>
+                  </div>
+                </div>
+                <div className="sidebar-right">
+                  <h2 className="title">Members</h2>
+                  <div className="members">
+
+                    {members.map((member, key) => {
+
+                      return (
+                        <div key={key} className="member">
+                          <div className="user-image">
+                            <img src={avatar} alt=""/>
+                          </div>
+                        <div className="member-info">
+                          <h2>{member.name}</h2>
+                          <p> Joined: 3 days ago </p>
+                        </div>
+
+                       </div>
+                      )
+                    })}
+
+                 </div>
+                </div>
               </div>
-          </div>
+            </div>
         )
     }
 }
---
+++ chatigloo-frontend/src/store.js
@@ -0,0 +1,86 @@
+import {OrderedMap} from 'immutable'
+// const { OrderedMap } = require('immutable')
+
+
+const users = OrderedMap({
+  '1': {_id: '1', name: "Carl", created: new Date()},
+  '2': {_id: '2', name: "Alexander", created: new Date()},
+  '3': {_id: '3', name: "Gelsey", created: new Date()},
+})
+
+export default class Store {
+  contructor(appComponent) {
+
+    this.app = appComponent;
+    this.messages = new OrderedMap()
+    this.channels = new OrderedMap()
+    this.activeChannelId = null;
+    this.user = {
+      _id: 0,
+      name: 'Carl',
+      created: new Date(),
+    }
+  }
+
+
+//Group Members Channel
+  setActiveChannelId(id){
+    this.activeChannel = id
+    this.update()
+  }
+  getActiveChannel() {
+
+        const channel = this.activeChannelId ? this.channels.get(this.activeChannelId) : this.channels.first();
+        return channel;
+
+}
+  addMessage(index, message = {}){
+    this.messages = this.messages.set(`${index}`, message);
+
+    this.update()
+  }
+  getMessages(){
+    return this.messages.valueSeq()
+
+  }
+  getMessagesFromChannel(channel){
+    let messages = []
+
+
+    if(channel){
+       channel.messages.map((value, key) => {
+         const message = this.messages.get(key)
+
+         messages.push(message)
+       });
+    }
+    return messages
+  }
+  getMembersFromChannel(channel){
+    const members = [];
+
+    if(channel){
+      channel.members.map((value, key) => {
+        console.log("Debug", key);
+
+        const member = users.get(key)
+
+        members.push(member)
+      })
+    }
+
+    return members
+  }
+  addChannel(index, channel = {}){
+    this.channels = this.channels.set(`${index}`, channel)
+
+    this.update()
+  }
+  getChannels(){
+    return this.channels.valueSeq()
+
+  }
+  update(){
+    this.app.forceUpdate()
+  }
+}
